b5fdddd570f3dedafdc21b47421432fb610351ff
JoaoGabriielDev <jgabriellima1067@gmail.com>
2024-08-29 22:23:17 -0300

    estrutura do html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TelaPrincipalComponent } from './components/tela-principal/tela-principal.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"dashboard",
+    component: TelaPrincipalComponent
+  }
+
+];
